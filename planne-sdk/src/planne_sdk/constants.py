5e976550f260a871e1d44ed1c07e8eac839630a9
vinivosh <36175157+vinivosh@users.noreply.github.com>
2026-02-15 20:09:26 -0300

    feat(sdk): Add `constants.py` file
    Oops, forgot to add this one earlier!

---
+++ planne-sdk/src/planne_sdk/constants.py
@@ -0,0 +1,44 @@
+"""Contains constants used throughout Planne SDK."""
+
+import os
+
+import dotenv
+
+# Loading ".env" file
+dotenv.load_dotenv()
+
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "WARN").upper()
+if LOG_LEVEL not in [
+    "CRITICAL",
+    "FATAL",
+    "ERROR",
+    "WARN",
+    "WARNING",
+    "INFO",
+    "DEBUG",
+]:
+    LOG_LEVEL = "WARN"
+
+
+# * ###########################################################################
+# * DB credentials
+# * ###########################################################################
+
+POSTGRES_SERVER = os.getenv("POSTGRES_SERVER", "postgres")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+POSTGRES_DB = os.getenv("POSTGRES_DB", "planne")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
+
+
+def get_postgres_uri():
+    """Build and return the PostgreSQL URI for SQLAlchemy."""
+    return f"postgresql+psycopg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
+# * ###########################################################################
+# * DB table names
+# * ###########################################################################
+
+USER_TABLE_NAME: str = "users"
