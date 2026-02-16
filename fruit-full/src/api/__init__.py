8260e33517fb82da2651b9b65dbc12d01bea11ef
vinivosh <36175157+vinivosh@users.noreply.github.com>
2026-02-16 03:47:59 -0300

    feat(fruit): Impl. User CRUD routes

--- fruit-full/src/api/__init__.py
+++ fruit-full/src/api/__init__.py
@@ -2,8 +2,9 @@
 
 from fastapi import APIRouter
 
-from api.routes import login
+from api.routes import login, user
 
 api_router = APIRouter()
 
 api_router.include_router(login.router, prefix="/login", tags=["login"])
+api_router.include_router(user.router, prefix="/user", tags=["user"])
