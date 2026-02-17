7518f20fd404b64c9f3dcb2cc13760a83d02585a
vinivosh <36175157+vinivosh@users.noreply.github.com>
2026-02-17 00:52:05 -0300

    test(sdk): Implement Pytest configurations and scaffolding

---
+++ planne-sdk/src/planne_sdk/tests/conftest.py
@@ -0,0 +1,49 @@
+import random
+
+import pytest
+from sqlmodel import Session, SQLModel, create_engine
+
+
+class DBSession:
+    """Context manager for creating and managing a simple SQLite DB session to use in tests."""
+
+    def __init__(self):
+        """Initialize DBSession, creating an in-memory SQLite DB and creating all the tables."""
+        self.engine = create_engine("sqlite:///:memory:", echo=False)
+        SQLModel.metadata.create_all(self.engine)
+
+    def __enter__(self) -> Session:
+        """Enter the context manager, returning the new DB session."""
+        self.session = Session(self.engine)
+        return self.session
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Exit the context manager, closing the DB session."""
+        self.session.close()
+
+    def clean_db(self):
+        """Clean the database by deleting all rows from all tables."""
+        with Session(self.engine) as session:
+            for table in reversed(SQLModel.metadata.sorted_tables):
+                session.exec(table.delete())
+            session.commit()
+
+
+@pytest.fixture(scope="session")
+def _db_session():
+    """Fixture to initialize the DB before test session.
+
+    Closes the DB session after the test session ends.
+    """
+    with DBSession() as session:
+        yield session
+
+
+@pytest.fixture()
+def db_session(_db_session):
+    """Fixture to clean the DB before each test, deleting all rows from all tables."""
+    for table in reversed(SQLModel.metadata.sorted_tables):
+        _db_session.exec(table.delete())
+    _db_session.commit()
+
+    yield _db_session
